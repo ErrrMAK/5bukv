fcd2f5ae6713ea3f9c3bbf9dd792ba454c7dab28
ErMAK <ermakov_kirill@mail.ru>
2022-12-10 22:57:22 +0100

    patch 1st day

--- ABC.py
+++ ABC.py
@@ -1,25 +1,21 @@
 from bs4 import BeautifulSoup
 import pandas as pd
 
-# tables2 = pd.read_html('https://ru.wikipedia.org/wiki/%D0%9F%D0%B0%D0%BD%D0%B4%D0%B5%D0%BC%D0%B8%D1%8F_COVID-19', match='стран')
-url = 'https://ru.wikipedia.org/wiki/%D0%A7%D0%B0%D1%81%D1%82%D0%BE%D1%82%D0%BD%D0%BE%D1%81%D1%82%D1%8C'
-tables = pd.read_html(url)
-# tables.drop(labels=None, axis=0, index=None, columns=None, level=None, inplace=False, errors='raise')
-df = tables[0]
-abc_tables = df[df.columns[[0, 1, 2]]]
 
-headers = abc_tables.columns.values.tolist()
-abc_tables = abc_tables.values.tolist()
-# abc_tables.insert(0, headers)
-
-
-def abc():
+def pars_wiki():
+    url = 'https://ru.wikipedia.org/wiki/%D0%A7%D0%B0%D1%81%D1%82%D0%BE%D1%82%D0%BD%D0%BE%D1%81%D1%82%D1%8C'
+    tables = pd.read_html(url)
+    df = tables[0]
+    abc_tables = df[df.columns[[0, 1, 2]]]
+    abc_tables = abc_tables.values.tolist()
     return abc_tables
 
-#Функция поиска частотности получаемой буквы
-def find_letter(letter):
+#глобальная переменная позволяет не ходить каждый раз в Вики
+abc_tables = pars_wiki()
+
+
+# Функция поиска частотности получаемой буквы
+def frequensy_by_letter(letter):
     for i in abc_tables:
         if letter == i[0][0]:
             return i[2]
-
-
--- import_words.py
+++ import_words.py
@@ -1,44 +1,48 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
-from ABC import find_letter
+from ABC import frequensy_by_letter
 
-
-# Парсим и подготавливаем базу
-url = "http://russkiyslovar.ru/iz-5-bukv"
-get = requests.get(url)
-soup = BeautifulSoup(get.text, 'lxml')
-s = soup.find('div', class_='field-item even').text
-
-base_Words = s.split()
-base_Words = base_Words[2:]
-done_words = []
-base_frequency = []
-
-# Очищаем базу от слов с дефисом
-for i in range(len(base_Words)):
-    if base_Words[i].find("-") == -1:
-        done_words.append(base_Words[i])
-
-# Присваиваем каждому слову ценность (сумма частотностей каждой буквы)
-for i in range(len(done_words)):
-    tmp_frequency = 0
-    tmp_word = ''
-    for j in range(len(done_words[i])):
-        if tmp_word.find(done_words[i][j]) == -1:
-            tmp_frequency += find_letter(done_words[i][j])
-        tmp_word += done_words[i][j]
-    base_frequency.append(tmp_frequency)
-
-base = {
-    'word': done_words,
-    'frequency': base_frequency,
-    'id': base_frequency,
-}
-
-# Сортируем базу
-base = pd.DataFrame(base).sort_values(by='id', ascending=False)
-base = list(base.set_index('id')['word'])
 
 def wordlist():
+    # Парсим и подготавливаем базу
+    url = "http://russkiyslovar.ru/iz-5-bukv"
+    get = requests.get(url)
+    soup = BeautifulSoup(get.text, 'lxml')
+    s = soup.find('div', class_='field-item even').text
+
+    base_Words = s.split()
+    base_Words = base_Words[2:]
+    done_words = []
+    base_frequency = []
+
+    # Очищаем базу от слов с дефисом
+    for i in range(len(base_Words)):
+        if base_Words[i].find("-") == -1:
+            done_words.append(base_Words[i])
+
+    # Присваиваем каждому слову ценность (сумма частотностей каждой буквы)
+    for word in done_words:
+        base_frequency.append(frequency_word(word))
+
+    base = {
+        'word': done_words,
+        'id': base_frequency,
+    }
+
+
+    # Сортируем базу
+    base = pd.DataFrame(base).sort_values(by='id', ascending=False)
+    base = list(base.set_index('id')['word'])
     return base
+
+
+def frequency_word(word):
+    frequency = 0
+    tmp_word = ''
+    for letter in word:
+        if tmp_word.find(letter) == -1:
+            frequency += frequensy_by_letter(letter)
+        tmp_word += letter
+    return frequency
+
